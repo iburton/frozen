94ace3276ed2d13ab7e7ea3a174589d8a94e3bd6
Ira Burton <ira.burton@gmail.com>
2019-12-13 22:42:32 -0700

    :heavy_plus_sign: fomantic/semantic-ui

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -5,6 +5,9 @@
 </template>
 
 <script>
+  window.$ = window.jQuery = require('jquery')
+  require('fomantic-ui/dist/semantic.min.css')
+  require('fomantic-ui/dist/semantic.min.js')
   export default {
     name: 'frozen'
   }
